7a86b85dcce3b7d3df33cd16f1ee6ff37be38675
Kristian Flatheim Jensen <kristianflatheim@hotmail.com>
2015-11-04 11:16:02 +0100

    up

--- arbitrator.py
+++ arbitrator.py
@@ -2,6 +2,10 @@ import motob_wheels
 import motob_camera
 
 class Arbitrator(object):
+	def __init__(self):
+		self.wheels = motob_wheels.MotobWheels()
+		self.camera = motob_camera.MotobCamera()
+
 	def do_best_action(self, behaviors):
 		best_behavior = behaviors[0]
 		for b in behaviors[1:]:
@@ -11,5 +15,5 @@ class Arbitrator(object):
 		camera_value = best_behavior.get_camera_value()
 		wheel_value = best_behavior.get_wheel_value()
 
-		motob_wheels.update(wheel_value)
-		motob_camera.update(camera_value)
+		self.wheels.update(wheel_value)
+		self.camera.update(camera_value)
